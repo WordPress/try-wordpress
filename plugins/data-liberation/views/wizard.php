e337c02b3e98ad6b2ff88638d4dba6b5d4adfa7a
Alex Kirk <akirk@users.noreply.github.com>
2024-07-26 13:02:09 +0200

    typo

--- plugins/data-liberation/views/wizard.php
+++ plugins/data-liberation/views/wizard.php
@@ -2,7 +2,7 @@
 	<h1><?php esc_html_e( 'Liberate Your Data', 'data-liberation' ); ?></h1>
     Site Title:
 	<input type="text" id="site-title" value="" disabled /><br/><br/>
-	<button id="start-import"><?php esc_html_e( 'Start Extraction', 'data-liberation' ); ?></button>
+	<button id="data-liberation-import"><?php esc_html_e( 'Start Extraction', 'data-liberation' ); ?></button>
 
     <h2><?php esc_html_e( 'Progress', 'data-liberation' ); ?></h2>
 
