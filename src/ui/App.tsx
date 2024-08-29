5a9d2ccaf327359a9520723d1eafe073e2832335
Paulo Pinto <paulo.pinto@automattic.com>
2024-08-29 15:53:51 +0100

    Set initial route

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,7 +1,7 @@
 import { Preview } from '@/ui/Preview';
 import {
 	createHashRouter,
-	createRoutesFromElements,
+	createRoutesFromElements, Navigate,
 	Outlet,
 	Route,
 	RouterProvider,
@@ -11,15 +11,19 @@ import { NewImport } from "@/ui/screens/NewImport";
 import { ViewImport } from "@/ui/screens/ViewImport";
 import { Home } from "@/ui/screens/Home";
 
-const router = createHashRouter(
-	createRoutesFromElements(
-		<Route path="/" element={<App />}>
-			<Route path="" element={<Home/>}/>
-			<Route path="new-import" element={<NewImport/>}/>
-			<Route path="view-import" element={<ViewImport/>}/>
-		</Route>,
-	),
-);
+function createRouter() {
+	const navigateTo = '/new-import';
+
+	return createHashRouter(
+		createRoutesFromElements(
+			<Route path="/" element={<App/>}>
+				<Route index element={navigateTo ? <Navigate to={navigateTo} replace/> : <Home/>}/>
+				<Route path="new-import" element={<NewImport/>}/>
+				<Route path="view-import" element={<ViewImport/>}/>
+			</Route>,
+		)
+	);
+}
 
 function App() {
 	return (
@@ -35,7 +39,7 @@ function App() {
 export function createApp() {
 	return (
 		<StrictMode>
-			<RouterProvider router={router} />
+			<RouterProvider router={createRouter()} />
 		</StrictMode>
 	);
 }
