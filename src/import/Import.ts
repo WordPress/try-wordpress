df7632a45f2baf9062f24df316c0dcc6e7edd376
Paulo Pinto <paulo.pinto@automattic.com>
2024-08-29 17:26:47 +0100

    Create an import

---
+++ src/import/Import.ts
@@ -0,0 +1,5 @@
+export interface Import {
+    id: string;
+    url: string;
+    title?: string;
+}
--- src/ui/screens/NewImport.tsx
+++ src/ui/screens/NewImport.tsx
@@ -1,16 +1,60 @@
 import { useNavigate } from "react-router-dom";
+import { Import } from "@/import/Import";
 
 export function NewImport() {
     const navigate = useNavigate();
-    const createImport = () => {
-        // TODO.
-        navigate(`/view-import`);
+    const handleClick = async () => {
+        try {
+            const import_ = await createImport();
+            navigate(`/view-import:${import_.id}`);
+        } catch (error) {
+            // TODO: Handle error.
+            console.error('Failed to create import', error);
+            return;
+        }
     };
 
     return (
         <>
             <p>Start by navigating to the main page of your site, then click Continue.</p>
-            <button onClick={() => createImport()}>Continue</button>
+            <button onClick={handleClick}>Continue</button>
         </>
     );
+}
+
+async function createImport(): Promise<Import> {
+    const info = await getSiteInfo();
+    if (!info) {
+        throw new Error('Failed to retrieve site info');
+    }
+
+    const import_: Import = {
+        id: Date.now().toString(16),
+        url: info.url,
+        title: info.title,
+    };
+
+    // TODO: Save in storage.
+
+    return import_;
+}
+
+async function getSiteInfo(): Promise<null|{
+    url: string;
+    title?: string;
+}> {
+    const tabs = await browser.tabs.query({currentWindow: true, active: true});
+    if (tabs.length !== 1) {
+        return null;
+    }
+    const tab = tabs[0];
+
+    if (!tab.url) {
+        return null;
+    }
+
+    return {
+        url: tab.url,
+        title: tab.title,
+    };
 }
