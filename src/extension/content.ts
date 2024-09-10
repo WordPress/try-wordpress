86fa031bc3525089c9fbd8b378af896b81126e72
Paulo Pinto <paulo.pinto@automattic.com>
2024-09-10 14:31:30 +0100

    Highlight element

--- src/extension/content.ts
+++ src/extension/content.ts
@@ -8,10 +8,14 @@ browser.runtime.onMessage.addListener(
 
 		switch ( message.action ) {
 			case Actions.EnableHighlighting:
-				console.log( message.action );
+				document.body.addEventListener( 'mouseover', onMouseOver );
+				document.body.addEventListener( 'mouseout', onMouseOut );
+				document.body.addEventListener( 'click', onClick );
 				break;
 			case Actions.DisableHighlighting:
-				console.log( message.action );
+				document.body.removeEventListener( 'mouseover', onMouseOver );
+				document.body.removeEventListener( 'mouseout', onMouseOut );
+				document.body.removeEventListener( 'click', onClick );
 				break;
 			default:
 				console.error( `Unknown action: ${ message.action }` );
@@ -19,3 +23,44 @@ browser.runtime.onMessage.addListener(
 		}
 	}
 );
+
+function onClick( event: MouseEvent ) {
+	event.preventDefault();
+}
+
+function onMouseOver( event: MouseEvent ) {
+	const element = event.target as HTMLElement | null;
+	if ( ! element ) {
+		return;
+	}
+	element.style.outline = '1px solid blue';
+	toggleCursorStyle();
+}
+
+function onMouseOut( event: MouseEvent ) {
+	const element = event.target as HTMLElement | null;
+	if ( ! element ) {
+		return;
+	}
+	element.style.outline = '';
+	toggleCursorStyle();
+}
+
+function toggleCursorStyle() {
+	const styleId = 'hover-highlighter-style';
+	let style = document.getElementById( styleId );
+	if ( style ) {
+		// If the style element exists, remove it
+		style.remove();
+	} else {
+		// If the style element does not exist, create and inject it
+		style = document.createElement( 'style' );
+		style.id = styleId;
+		style.textContent = `
+		*:hover {
+			cursor: crosshair !important;
+		}
+	  `;
+		document.head.append( style );
+	}
+}
