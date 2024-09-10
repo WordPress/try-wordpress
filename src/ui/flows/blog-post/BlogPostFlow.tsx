a08465b289b81f3bd641adc15fb00d0306a1344a
Paulo Pinto <paulo.pinto@automattic.com>
2024-09-10 17:53:50 +0100

    Split into multiple components

--- src/ui/flows/blog-post/BlogPostFlow.tsx
+++ src/ui/flows/blog-post/BlogPostFlow.tsx
@@ -2,85 +2,85 @@ import { useState } from 'react';
 import { ContentApi } from '@/api/ContentApi';
 
 enum Steps {
-	Start = 1,
-	SelectTitle,
-	SelectContent,
-	Import,
+	start = 1,
+	selectContent,
+	import,
 }
 
 const contentApi = new ContentApi();
 
 export function BlogPostFlow() {
-	const [ currentStep, setCurrentStep ] = useState( Steps.Start );
+	const [ currentStep, setCurrentStep ] = useState( Steps.start );
 
-	const startElement =
-		currentStep !== Steps.Start ? null : (
-			<>
-				<div>
-					Navigate to the page of the post you&apos;d like to import
-				</div>
-				<button
-					onClick={ async () => {
-						await contentApi.enableHighlighting();
-						setCurrentStep( Steps.SelectTitle );
-					} }
-				>
-					Continue
-				</button>
-			</>
-		);
+	const startElement = (
+		<Start onExit={ () => setCurrentStep( Steps.selectContent ) } />
+	);
 
-	const selectTitleElement =
-		currentStep !== Steps.SelectTitle ? null : (
-			<>
-				<div>Select the title of the post</div>
-				<button
-					onClick={ () => {
-						setCurrentStep( Steps.SelectContent );
-					} }
-				>
-					Continue
-				</button>
-			</>
-		);
+	const selectContentElement = (
+		<SelectContent onExit={ () => setCurrentStep( Steps.import ) } />
+	);
 
-	const selectContentElement =
-		currentStep !== Steps.SelectContent ? null : (
-			<>
-				<div>Select the content of the post</div>
-				<button
-					onClick={ async () => {
-						await contentApi.disableHighlighting();
-						setCurrentStep( Steps.Import );
-					} }
-				>
-					Continue
-				</button>
-			</>
-		);
-
-	const importElement =
-		currentStep !== Steps.Import ? null : (
-			<>
-				<div>Click import to import the post</div>
-				<button
-					onClick={ () => {
-						console.log( 'import' );
-					} }
-				>
-					Import
-				</button>
-			</>
-		);
+	const importElement = <Import onExit={ () => console.log( 'import' ) } />;
 
 	return (
 		<>
-			{ currentStep === Steps.Start ? startElement : null }
-			{ currentStep === Steps.SelectTitle ? selectTitleElement : null }
-			{ currentStep === Steps.SelectContent
+			{ currentStep === Steps.start ? startElement : null }
+			{ currentStep === Steps.selectContent
 				? selectContentElement
 				: null }
-			{ currentStep === Steps.Import ? importElement : null }
+			{ currentStep === Steps.import ? importElement : null }
+		</>
+	);
+}
+
+function Start( props: { onExit: () => void } ) {
+	const { onExit } = props;
+	return (
+		<>
+			<div>
+				Navigate to the page of the post you&apos;d like to import
+			</div>
+			<button
+				onClick={ async () => {
+					await contentApi.enableHighlighting();
+					onExit();
+				} }
+			>
+				Continue
+			</button>
+		</>
+	);
+}
+
+function SelectContent( props: { onExit: () => void } ) {
+	const { onExit } = props;
+	return (
+		<>
+			<div>Select the content of the post</div>
+			<button
+				onClick={ async () => {
+					await contentApi.disableHighlighting();
+					onExit();
+				} }
+			>
+				Continue
+			</button>
+		</>
+	);
+}
+
+function Import( props: { onExit: () => void } ) {
+	const { onExit } = props;
+	return (
+		<>
+			<div>Click import to import the post</div>
+			<button
+				onClick={ () => {
+					onExit();
+				} }
+			>
+				Import
+			</button>
 		</>
 	);
 }
