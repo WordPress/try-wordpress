5b56874d414843ad74287562f987580d7c4607a3
Paulo Pinto <paulo.pinto@automattic.com>
2024-09-11 14:21:41 +0100

    Use a button instead of a link

--- src/ui/session/ViewSession.tsx
+++ src/ui/session/ViewSession.tsx
@@ -1,11 +1,12 @@
 import { useSessionContext } from '@/ui/session/SessionProvider';
 import { Post } from '@/api/ApiClient';
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Screens } from '@/ui/App';
 
 export function ViewSession() {
 	const { session, apiClient } = useSessionContext();
+	const navigate = useNavigate();
 
 	const [ posts, setPosts ] = useState< Post[] >( [] );
 	useEffect( () => {
@@ -25,9 +26,13 @@ export function ViewSession() {
 			</h1>
 			<ul>
 				<li>
-					<Link to={ Screens.flowBlogPost( session.id ) }>
+					<button
+						onClick={ () =>
+							navigate( Screens.flowBlogPost( session.id ) )
+						}
+					>
 						Import Blog Post
-					</Link>
+					</button>
 				</li>
 			</ul>
 		</>
