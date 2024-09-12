581ae6b302dd3de5b64d81f7369c025a5836cb1b
Paulo Pinto <paulo.pinto@automattic.com>
2024-09-12 11:39:54 +0100

    Remove unused code

--- src/ui/session/ViewSession.tsx
+++ src/ui/session/ViewSession.tsx
@@ -1,23 +1,10 @@
 import { useSessionContext } from '@/ui/session/SessionProvider';
-import { Post } from '@/api/ApiClient';
-import { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Screens } from '@/ui/App';
 
 export function ViewSession() {
-	const { session, apiClient } = useSessionContext();
+	const { session } = useSessionContext();
 	const navigate = useNavigate();
-
-	const [ posts, setPosts ] = useState< Post[] >( [] );
-	useEffect( () => {
-		if ( ! apiClient ) {
-			return;
-		}
-		const getPosts = async () => {
-			setPosts( await apiClient.getPosts() );
-		};
-		void getPosts();
-	}, [ apiClient?.siteUrl ] );
 
 	return (
 		<>
