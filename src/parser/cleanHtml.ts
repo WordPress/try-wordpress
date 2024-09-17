fd4307b2036b103fbeba7f833f13ea3a7998c61d
Paulo Pinto <paulo.pinto@automattic.com>
2024-09-17 14:51:27 +0100

    Serialize blocks instead of INLINE mode

--- src/parser/cleanHtml.ts
+++ src/parser/cleanHtml.ts
@@ -1,12 +1,13 @@
-import { pasteHandler } from '@wordpress/blocks';
+import { pasteHandler, serialize } from '@wordpress/blocks';
 import { registerCoreBlocks } from '@wordpress/block-library';
 
 export function cleanHtml( html: string ): string {
 	init();
-	return pasteHandler( {
-		mode: 'INLINE',
+	const blocks = pasteHandler( {
+		mode: 'BLOCKS',
 		HTML: html,
 	} );
+	return serialize( blocks );
 }
 
 let isInitialized = false;
