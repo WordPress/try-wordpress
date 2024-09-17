78b71136530503e51e07504f31b96e5423c947e1
Paulo Pinto <paulo.pinto@automattic.com>
2024-09-17 14:33:05 +0100

    Call registerCoreBlocks()

--- src/parser/cleanHtml.ts
+++ src/parser/cleanHtml.ts
@@ -1,8 +1,19 @@
 import { pasteHandler } from '@wordpress/blocks';
+import { registerCoreBlocks } from '@wordpress/block-library';
 
 export function cleanHtml( html: string ): string {
+	init();
 	return pasteHandler( {
 		mode: 'INLINE',
 		HTML: html,
 	} );
+}
+
+let isInitialized = false;
+function init() {
+	if ( isInitialized ) {
+		return;
+	}
+	registerCoreBlocks();
+	isInitialized = true;
 }
