28a57dd62a84117137642fd0d6ac80348b692e6d
Paulo Pinto <paulo.pinto@automattic.com>
2024-09-24 14:56:11 +0100

    Add session to breadcrumbs

--- src/ui/breadcrumbs/Breadcrumbs.tsx
+++ src/ui/breadcrumbs/Breadcrumbs.tsx
@@ -1,15 +1,27 @@
-import { useNavigate } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Screens } from '@/ui/App';
+import { useSessionContext } from '@/ui/session/SessionProvider';
 
 export function Breadcrumbs( props: { className: string } ) {
 	const { className } = props;
-	const navigate = useNavigate();
+	const { session } = useSessionContext();
+	const location = useLocation();
+
+	const showSession = location.pathname.startsWith( '/session' );
+
 	return (
 		<nav className={ className }>
 			<ul>
-				<button onClick={ () => navigate( Screens.home() ) }>
-					Home
-				</button>
+				<li>
+					<Link to={ Screens.home() }>Home</Link>
+				</li>
+				{ ! showSession ? null : (
+					<li>
+						<Link to={ Screens.viewSession( session.id ) }>
+							{ session.title }
+						</Link>
+					</li>
+				) }
 			</ul>
 		</nav>
 	);
