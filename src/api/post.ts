b1a6702e6588920c11ae32dd12eb792bae3c4753
Paulo Pinto <paulo.pinto@automattic.com>
2024-09-25 13:59:37 +0100

    Implement apiResponseToPost()

---
+++ src/api/post.ts
@@ -0,0 +1,33 @@
+/* eslint-disable camelcase */
+import { WP_REST_API_Post } from 'wp-types';
+export interface ApiPost extends WP_REST_API_Post {}
+/* eslint-enable camelcase */
+
+import { Post, PostContent, PostDate, PostTitle } from '@/model/Post';
+
+interface PostMeta {
+	guid: string;
+	raw_title: string;
+	raw_date: string;
+	raw_content: string;
+}
+
+export function apiResponseToPost( response: ApiPost ): Post {
+	const meta = response.meta as unknown as PostMeta;
+
+	const date = new PostDate( response.date_gmt, meta.raw_date );
+	const title = new PostTitle( response.title.raw ?? '', meta.raw_title );
+	const content = new PostContent(
+		response.content.raw ?? '',
+		meta.raw_content
+	);
+
+	return {
+		guid: meta.guid,
+		id: response.id,
+		url: response.link,
+		date,
+		content,
+		title,
+	};
+}
