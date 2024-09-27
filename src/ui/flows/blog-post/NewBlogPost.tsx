5339d63a8bf3d942f01fb694357c99925937e63c
Paulo Pinto <paulo.pinto@automattic.com>
2024-09-27 14:03:30 +0100

    Use Toolbar in NewBlogPost

--- src/ui/flows/blog-post/NewBlogPost.tsx
+++ src/ui/flows/blog-post/NewBlogPost.tsx
@@ -3,6 +3,7 @@ import { ContentBus } from '@/bus/ContentBus';
 import { useSessionContext } from '@/ui/session/SessionProvider';
 import { useNavigate } from 'react-router-dom';
 import { Screens } from '@/ui/App';
+import { Toolbar } from '@/ui/flows/blog-post/Toolbar';
 
 export function NewBlogPost() {
 	const navigate = useNavigate();
@@ -29,27 +30,30 @@ export function NewBlogPost() {
 
 	const element = (
 		<>
+			<Toolbar>
+				<button
+					onClick={ async () => {
+						const currentPage =
+							await ContentBus.getCurrentPageInfo();
+						let post = await apiClient!.posts.findByGuid(
+							currentPage.url
+						);
+						if ( ! post ) {
+							post = await apiClient!.posts.create( {
+								guid: currentPage.url,
+							} );
+						}
+						navigate(
+							Screens.flows.blogPost.edit( session.id, post.id )
+						);
+					} }
+				>
+					Continue
+				</button>
+			</Toolbar>
 			<div>
 				Navigate to the page of the post you&apos;d like to import
 			</div>
-			<button
-				onClick={ async () => {
-					const currentPage = await ContentBus.getCurrentPageInfo();
-					let post = await apiClient!.posts.findByGuid(
-						currentPage.url
-					);
-					if ( ! post ) {
-						post = await apiClient!.posts.create( {
-							guid: currentPage.url,
-						} );
-					}
-					navigate(
-						Screens.flows.blogPost.edit( session.id, post.id )
-					);
-				} }
-			>
-				Continue
-			</button>
 		</>
 	);
 
