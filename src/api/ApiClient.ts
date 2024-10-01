e2cc03506bf1e407eaa8213ed91c194b4da5e74a
Paulo Pinto <paulo.pinto@automattic.com>
2024-10-01 17:58:47 +0100

    Implement PostsApi

--- src/api/ApiClient.ts
+++ src/api/ApiClient.ts
@@ -1,8 +1,8 @@
-/* eslint-disable react/no-is-mounted */
 import { PlaygroundClient } from '@wp-playground/client';
 import { BlogPostsApi } from '@/api/BlogPosts';
 import { SettingsApi } from '@/api/Settings';
 import { UsersApi } from '@/api/Users';
+import { PostsApi } from '@/api/Posts';
 
 export class ApiClient {
 	private readonly playgroundClient: PlaygroundClient;
@@ -10,10 +10,12 @@ export class ApiClient {
 	private readonly _blogPosts: BlogPostsApi;
 	private readonly _settings: SettingsApi;
 	private readonly _users: UsersApi;
+	private readonly _posts: PostsApi;
 
 	constructor( playgroundClient: PlaygroundClient, siteUrl: string ) {
 		this.playgroundClient = playgroundClient;
 		this._siteUrl = siteUrl;
+		this._posts = new PostsApi( this );
 		this._blogPosts = new BlogPostsApi( this );
 		this._settings = new SettingsApi( this );
 		this._users = new UsersApi( this );
@@ -21,6 +23,10 @@ export class ApiClient {
 
 	get siteUrl(): string {
 		return this._siteUrl;
+	}
+
+	get posts(): PostsApi {
+		return this._posts;
 	}
 
 	get blogPosts(): BlogPostsApi {
@@ -72,5 +78,3 @@ export class ApiClient {
 		return response.json;
 	}
 }
-
-/* eslint-enable react/no-is-mounted */
---
+++ src/api/Posts.ts
@@ -0,0 +1,47 @@
+/* eslint-disable camelcase */
+import { WP_REST_API_Post } from 'wp-types';
+type ApiPost = WP_REST_API_Post;
+/* eslint-enable camelcase */
+
+import { ApiClient } from '@/api/ApiClient';
+import { Post } from '@/model/content/Post';
+
+interface PostMeta {
+	guid: string;
+}
+
+export class PostsApi {
+	// eslint-disable-next-line no-useless-constructor
+	constructor( private readonly client: ApiClient ) {}
+
+	async findById( id: string ): Promise< Post | null > {
+		// eslint-disable-next-line react/no-is-mounted
+		const posts = await this.find( { id } );
+		return posts.length === 0 ? null : fromApiResponse( posts[ 0 ] );
+	}
+
+	private async find(
+		params: Record< string, string >
+	): Promise< ApiPost[] > {
+		// A liberated_post is always draft.
+		params.status = 'draft';
+
+		// TODO: For now we're querying liberated_posts but this must be changed as this call needs to retrieve
+		//       any kind of post.
+		return ( await this.client.get(
+			`/liberated_posts`,
+			params
+		) ) as ApiPost[];
+	}
+}
+
+function fromApiResponse( response: ApiPost ): Post {
+	const meta = response.meta as unknown as PostMeta;
+	return {
+		type: response.type,
+		guid: meta.guid,
+		id: response.id,
+		url: response.link,
+		fields: {},
+	};
+}
