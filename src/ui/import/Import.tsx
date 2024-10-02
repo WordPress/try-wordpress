28ea7ef84857f511ab29f13cc775564e14b241cd
Paulo Pinto <paulo.pinto@automattic.com>
2024-10-02 17:55:20 +0100

    Display the selectors in the Import screen

--- src/ui/import/Import.tsx
+++ src/ui/import/Import.tsx
@@ -1,7 +1,48 @@
 import { useParams } from 'react-router-dom';
+import { useBlueprint } from '@/ui/blueprints/useBlueprint';
+import { humanReadablePostType } from '@/model/content/Post';
+import { Toolbar } from '@/ui/blueprints/Toolbar';
+import { ReactElement } from 'react';
 
 export function Import() {
 	const params = useParams();
 	const blueprintId = params.blueprintId!;
-	return <>{ blueprintId }</>;
+	const [ blueprint ] = useBlueprint( blueprintId );
+
+	const fields: ReactElement[] = [];
+	if ( blueprint ) {
+		for ( const [ name, field ] of Object.entries( blueprint.fields ) ) {
+			fields.push(
+				<li key={ name }>
+					{ name }: { field.selector }
+				</li>
+			);
+		}
+	}
+
+	return (
+		<>
+			{ ! blueprint ? (
+				'Loading...'
+			) : (
+				<>
+					<Toolbar>
+						<button
+							onClick={ async () => {
+								console.log( 'TODO' );
+							} }
+						>
+							Continue (TODO)
+						</button>
+					</Toolbar>
+					We&apos;ll now import{ ' ' }
+					{ humanReadablePostType.get( blueprint.type ) }s using the
+					following selectors:
+					<br />
+					<br />
+					<ul>{ fields }</ul>
+				</>
+			) }
+		</>
+	);
 }
