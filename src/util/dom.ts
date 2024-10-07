1b7dd8f752d4aebd41dc8e7fc9a9b8ca5af1e91e
Paulo Pinto <paulo.pinto@automattic.com>
2024-10-07 18:17:54 +0100

    Add function to get selectors
    Copied from https://github.com/Automattic/tours/blob/trunk/assets/js/tour-step-editor.js#L161-L196

--- src/util/dom.ts
+++ src/util/dom.ts
@@ -17,3 +17,47 @@ export function findDeepestChild( html: string ): Element | undefined {
 	}
 	return deepestChild;
 }
+
+export function getSelectors( elem: HTMLElement ) {
+	const selectors = [];
+
+	while ( elem.parentElement ) {
+		const currentElement = elem.parentElement;
+		const tagName = elem.tagName.toLowerCase();
+		const classes = < string[] >[];
+
+		if ( elem.id ) {
+			selectors.push( tagName + '#' + elem.id );
+			break;
+		}
+
+		elem.classList.forEach( function ( c ) {
+			if ( ! filterSelectors( c ) ) {
+				return;
+			}
+			classes.push( c );
+		} );
+
+		if ( classes.length ) {
+			selectors.push( tagName + '.' + classes.join( '.' ) );
+		} else {
+			const index =
+				Array.prototype.indexOf.call( currentElement.children, elem ) +
+				1;
+			selectors.push( tagName + ':nth-child(' + index + ')' );
+		}
+
+		elem = currentElement;
+	}
+
+	return selectors.reverse();
+}
+
+function filterSelectors( c: string ): boolean {
+	return (
+		c.indexOf( 'wp-' ) > -1 ||
+		c.indexOf( 'page' ) > -1 ||
+		c.indexOf( 'post' ) > -1 ||
+		c.indexOf( 'column' ) > -1
+	);
+}
