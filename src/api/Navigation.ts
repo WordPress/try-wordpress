d23a9d77dee221d40c034c1f9cff0a6af55f2846
Paulo Pinto <paulo.pinto@automattic.com>
2024-10-24 17:13:33 +0100

    Add NavigationApi

---
+++ src/api/Navigation.ts
@@ -0,0 +1,6 @@
+import { ApiClient } from '@/api/ApiClient';
+
+export class NavigationApi {
+	// eslint-disable-next-line no-useless-constructor
+	constructor( private readonly client: ApiClient ) {}
+}
