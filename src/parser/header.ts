c5d80ba166a2a9c679bf8d1baf6de2a4ceaa5327
Paulo Pinto <paulo.pinto@automattic.com>
2024-10-31 14:05:44 +0000

    Implement header parser

--- src/parser/header.ts
+++ src/parser/header.ts
@@ -1,6 +1,19 @@
 import { Field } from '@/model/field/Field';
+import { serializeBlocks } from '@/parser/util';
+import {
+	NavigationField,
+	newNavigationField,
+} from '@/model/field/NavigationField';
 
 export function parseHeaderField( name: string, field: Field ): Field {
-	// TODO.
-	return field;
+	switch ( name ) {
+		case 'navigation':
+			return parseNavigation( field.original );
+		default:
+			throw Error( `unknown field type ${ field.type }` );
+	}
+}
+
+function parseNavigation( html: string ): NavigationField {
+	return newNavigationField( html, serializeBlocks( html ) );
 }
