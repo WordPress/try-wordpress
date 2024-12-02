9e8fb98f33a6435b4daeb82817909c8683b280c8
Paulo Pinto <paulo.pinto@automattic.com>
2024-12-02 17:58:41 +0000

    Make BlogPostBluePrintEditor use the schema fields

--- src/ui/blueprints/blog-post/BlogPostBlueprintEditor.tsx
+++ src/ui/blueprints/blog-post/BlogPostBlueprintEditor.tsx
@@ -2,6 +2,7 @@ import { Field } from '@/model/field/Field';
 import { BlogPost } from '@/model/subject/BlogPost';
 import { BlogPostBlueprint } from '@/model/blueprint/BlogPost';
 import { FieldsEditor } from '@/ui/components/FieldsEditor/FieldsEditor';
+import { getSchemaFields } from '@/schemas/fields';
 
 interface Props {
 	blueprint: BlogPostBlueprint;
@@ -11,30 +12,25 @@ interface Props {
 
 export function BlogPostBlueprintEditor( props: Props ) {
 	const { blueprint, subject, onFieldChanged } = props;
+	const schemaFields = getSchemaFields( subject.type );
 
-	const subjectFields: { name: string; field: Field }[] = [
-		{ name: 'title', field: subject.fields.title },
-		{ name: 'date', field: subject.fields.date },
-		{ name: 'content', field: subject.fields.content },
-	];
-
+	const subjectFields: { name: string; field: Field }[] = [];
 	const selectors: {
 		name: string;
 		selector?: string;
-	}[] = [
-		{
-			name: 'title',
-			selector: blueprint.fields.title.selector,
-		},
-		{
-			name: 'date',
-			selector: blueprint.fields.date.selector,
-		},
-		{
-			name: 'content',
-			selector: blueprint.fields.content.selector,
-		},
-	];
+	}[] = [];
+
+	Object.keys( schemaFields ).forEach( ( name ) => {
+		subjectFields.push( {
+			name,
+			field: subject.fields[ name ],
+		} );
+		selectors.push( {
+			name,
+			// @ts-ignore
+			selector: blueprint.fields[ name ].selector,
+		} );
+	} );
 
 	return (
 		<FieldsEditor
